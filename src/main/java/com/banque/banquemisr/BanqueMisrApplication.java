3b119b06ec2b2af31747d9e6b36fe6dcd046eb17
Eric Arden <ardeneric77@gmail.com>
2024-04-27 19:18:52 +0000

    Initialized Project...

---
+++ src/main/java/com/banque/banquemisr/BanqueMisrApplication.java
@@ -0,0 +1,13 @@
+package com.banque.banquemisr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanqueMisrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanqueMisrApplication.class, args);
+	}
+
+}

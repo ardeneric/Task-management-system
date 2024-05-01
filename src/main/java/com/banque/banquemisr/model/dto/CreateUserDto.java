2152a4cc8ddc7b29b69c81fd6902b848cb3908d4
Eric Arden <ardeneric77@gmail.com>
2024-05-01 01:45:58 +0000

    * Added validation for create user dto

--- src/main/java/com/banque/banquemisr/model/dto/CreateUserDto.java
+++ src/main/java/com/banque/banquemisr/model/dto/CreateUserDto.java
@@ -1,12 +1,17 @@
 package com.banque.banquemisr.model.dto;
 
 import com.banque.banquemisr.enums.UserRole;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateUserDto {
+    @NotNull(message = "Username is mandatory")
     private String username;
+    @NotNull(message = "Password is mandatory")
     private String password;
+    @NotNull(message = "Rolse is mandatory, ADMIN or USER")
     private UserRole role;
+    @NotNull(message = "Email is mandatory")
     private String email;
 }
